ee2ec688a5075ac68586f75432c9bbb855512321
Coignard Hervé <herve.coignard@pims.io>
2017-07-05 10:30:35 +0200

    Initialisation du projet. Initialisation de composer et creation des .gitignore. Création de la classe. Ajout des fonction(non dévelloper).

---
+++ src/StringFormatter.php
@@ -0,0 +1,30 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: herve
+	 * Date: 05/07/17
+	 * Time: 10:17
+	 */
+	
+	
+	class StringFormatter {
+		public function concat(string $str1, string $str2):string {
+			$result = "";
+			return $result;
+		}
+		
+		public function toCamelCase(string $str):string {
+			$result = "";
+			return $result;
+		}
+		
+		public function prefix(string $str1, string $str2):string {
+			$result = "";
+			return $result;
+		}
+		
+		public function suffix(string $sr2, string $str2):string {
+			$result = "";
+			return $result;
+		}
+	}
